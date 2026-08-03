1a913a22b199f1b6e8e41ebace014c409bbb427b
Alex Vanderbist <alex.vanderbist@gmail.com>
2026-08-03 14:56:26 +0200

    Initial commit

---
+++ Riffle/RiffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiffleApp.swift
+//  Riffle
+//
+//  Created by Alex Vanderbist on 03/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct RiffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
